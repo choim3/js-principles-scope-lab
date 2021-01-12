f1bf980eb6fddb99771a6ffd68a3b4c83a376b33
choim3 <choim3@spu.edu>
2021-01-11 17:27:45 -0800

    commit tree 3b566564ba636bcf6c172ba4e3d5607c386c1ded

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "libre";
+
+function upperCaseCustomerName() {
+  let upName = customerName.toUpperCase();
+  return upName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Nacho Libre";
+}
